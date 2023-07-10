9743413549b8fcb3a1d9eac985be77a7a4085cf3
Sue Roy <sueroy08@gmail.com>
2023-07-09 18:58:01 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+document.body.animate(
+  [
+    { backgroundColor: "red" },
+    { backgroundColor: "orange" },
+    { backgroundColor: "yellow" },
+    { backgroundColor: "limegreen" },
+    { backgroundColor: "blue" },
+    { backgroundColor: "purple" },
+    { backgroundColor: "red" },
+  ],
+  {
+    duration: 20000,
+    iterations: Infinity,
+  }
+);
